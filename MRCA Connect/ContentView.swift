f0ece4d66837ec3d0b51cac0bf4c20798ae060b9
Swapnil <swapnilandhale3@gmail.com>
2024-10-15 13:36:09 -0400

    Initial Commit

---
+++ MRCA Connect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MRCA Connect
+//
+//  Created by Swapnil on 2024-10-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MRCA Connect/MRCA_ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MRCA_ConnectApp.swift
+//  MRCA Connect
+//
+//  Created by Swapnil on 2024-10-15.
+//
+
+import SwiftUI
+
+@main
+struct MRCA_ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
